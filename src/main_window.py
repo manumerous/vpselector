caa724f998595dedaec27934e97bc35bc058cd70
Manumerous <manuel.galliker@gmx.ch>
2021-05-02 16:27:56 +0200

    Added Histogram plots The Visual Dataframe Selector now includes a histogram plot next to every subplot. This way the user can get a rough idea about the distribution of the data.

--- src/main_window.py
+++ src/main_window.py
@@ -13,14 +13,14 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 from matplotlib.widgets import SpanSelector
 
 import pandas as pd
-import sys
+import seaborn as sns
 
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, data_df, plot_config_dict, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.setWindowTitle("Visual Pandas Cropper")
+        self.setWindowTitle("Visual Dataframe Selector")
 
         self.data_df = data_df
         self.cropped_data_df = pd.DataFrame()
@@ -38,9 +38,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setup_plots()
 
         # Create toolbar for simultaneous manipulation of all subplots
-        self.addToolBar(NavigationToolbar(self.plt.canvas, self))
+        self.addToolBar(NavigationToolbar(self.data_plt.canvas, self))
 
-        layout = QtWidgets.QVBoxLayout()
+        w = QtWidgets.QWidget()
+        layout = QtWidgets.QGridLayout(w)
 
         text_label = QLabel(
             "Select Data by clicking the left mouse button and dragging the cursor")
@@ -51,10 +52,16 @@ class MainWindow(QtWidgets.QMainWindow):
         # connect signal
         self.termination_button.clicked.connect(self._terminate)
 
-        layout.addWidget(text_label)
-        layout.addWidget(self.plt)
-        layout.addWidget(self.termination_button,
+        layout.addWidget(text_label, 0, 0)
+        layout.addWidget(self.data_plt, 1, 1)
+        layout.addWidget(self.hist_plt, 1, 0)
+        layout.addWidget(self.termination_button, 2, 1,
                          alignment=QtCore.Qt.AlignRight)
+
+        layout.setColumnMinimumWidth(0, 300)
+        layout.setColumnMinimumWidth(1, 300)
+        layout.setColumnStretch(0, 1)
+        layout.setColumnStretch(1, 3)
 
         widget = QtWidgets.QWidget()
         widget.setLayout(layout)
@@ -64,23 +71,27 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def setup_plots(self):
 
-        subplot_keys = list(self.plot_config_dict.keys())
-        subplot_count = len(subplot_keys)
+        self.subplot_keys = list(self.plot_config_dict.keys())
+        self.subplot_count = len(self.subplot_keys)
 
-        self.plt = MplWidget(self, subplot_count=subplot_count)
+        self.data_plt = MplWidget(self, subplot_count=self.subplot_count)
+        self.hist_plt = MplWidget(self, subplot_count=self.subplot_count)
 
-        for i in range(subplot_count):
+        for i in range(self.subplot_count):
 
-            subplot_key = subplot_keys[i]
+            subplot_key = self.subplot_keys[i]
             subplot_topics_list = self.plot_config_dict[subplot_key]
             for topic in subplot_topics_list:
-                self.plt.canvas.subplot_axes[i].plot(
+                self.data_plt.canvas.subplot_axes[i].plot(
                     self.data_df[self.x_axis_col], self.data_df[topic], label=topic)
 
-                self.plt.canvas.subplot_axes[i].legend()
+                self.data_plt.canvas.subplot_axes[i].legend()
 
-            self.plt.canvas.subplot_axes[i].span = SpanSelector(
-                self.plt.canvas.subplot_axes[i],
+            sns.histplot(self.data_df[subplot_topics_list],
+                         ax=self.hist_plt.canvas.subplot_axes[i], stat="probability")
+
+            self.data_plt.canvas.subplot_axes[i].span = SpanSelector(
+                self.data_plt.canvas.subplot_axes[i],
                 onselect=self.on_region_select_callback,
                 direction="horizontal",
                 minspan=1,
@@ -88,20 +99,33 @@ class MainWindow(QtWidgets.QMainWindow):
                 span_stays=False,
                 button=[1],
                 rectprops={"facecolor": "green", "alpha": 0.3})
+        return
 
-        self.plt.show
+    def update_hist_plot(self, df):
+        for i in range(self.subplot_count):
+            self.hist_plt.canvas.subplot_axes[i].clear()
+            subplot_key = self.subplot_keys[i]
+            subplot_topics_list = self.plot_config_dict[subplot_key]
+            sns.histplot(df[subplot_topics_list],
+                         ax=self.hist_plt.canvas.subplot_axes[i], stat="probability")
+        self.hist_plt.canvas.draw()
+        return
 
     def on_region_select_callback(self, min_x_val, max_x_val):
-        self.plt.canvas.subplot_axes[0].set_xlim([min_x_val, max_x_val])
+        self.data_plt.canvas.subplot_axes[0].set_xlim([min_x_val, max_x_val])
+        cropped_df = self.crop_df(min_x_val, max_x_val)
+        self.update_hist_plot(cropped_df)
         print(min_x_val, max_x_val)
         dialog_window = ConfirmSelectionWindow()
         selection_accepted = dialog_window.exec_()
 
-        self.plt.canvas.subplot_axes[0].set_xlim([self.x_start, self.x_end])
+        self.data_plt.canvas.subplot_axes[0].set_xlim(
+            [self.x_start, self.x_end])
+        self.data_plt.canvas.draw()
+        self.update_hist_plot(self.data_df)
 
         if selection_accepted:
             print("selection accepted: ", min_x_val, max_x_val)
-            cropped_df = self.crop_df(min_x_val, max_x_val)
             cropped_df["old_index"] = cropped_df.index
             self.cropped_data_df = self.cropped_data_df.append(cropped_df)
             self.cropped_data_df = self.cropped_data_df.reset_index(drop=True)
