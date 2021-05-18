26d9521eecc8681f110def6daa04dcb4ccb28f97
Manumerous <manuel.galliker@gmx.ch>
2021-05-19 00:17:25 +0200

    Added absolute file path to testing file By merging the relative path of the log with the absolute path of the project folder it can be ensured that test_visual_dataframe_Selector works properly no matter from where it is run. This is very useful for pytest routines when including the vidsual dataframe selector somewhere in your project.

--- test_visual_dataframe_selector.py
+++ test_visual_dataframe_selector.py
@@ -3,6 +3,8 @@ __maintainer__ = "Manuel Galliker"
 __license__ = "Apache-2.0"
 
 import pandas as pd
+from pathlib import Path
+
 
 try:
     from data_selector import select_visual_data
@@ -11,11 +13,14 @@ except:
 
 
 def test_visual_dataframe_selector():
+    test_file_path = str(Path(__file__).parent.absolute()
+                         ) + "/resources/test.csv"
+    print(test_file_path)
     plot_config_dict = {
         "x_axis_col": "timestamp",
         "sub_plt1_data": ["q0", "q1", "q2", "q3"],
         "sub_plt2_data": ["u0", "u1", "u2", "u3", "u4", "u5", "u6", "u7"]}
-    data_df = pd.read_csv("resources/test.csv", index_col=0)
+    data_df = pd.read_csv(test_file_path, index_col=0)
 
     selected_df = select_visual_data(data_df, plot_config_dict)
     print("Selected dataframe:")
