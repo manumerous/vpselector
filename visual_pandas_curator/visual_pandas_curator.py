f45c9bf9fa52afbc5122c105a6ebc966ed544a75
Manuel Yves Galliker <manuel.galliker@gmx.ch>
2023-07-22 15:08:03 +0200

    Add type specifications in visual_pandas_curator

--- visual_pandas_curator/visual_pandas_curator.py
+++ visual_pandas_curator/visual_pandas_curator.py
@@ -11,16 +11,16 @@ import sys
 from visual_pandas_curator.windows.main_window import MainWindow
 
 # use this function if no pyqt app is running
-def select_visual_data(data_df, plot_config_dict):
+def select_visual_data(data : pd.DataFrame , plot_config : dict):
     app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow(data_df, plot_config_dict)
+    w = MainWindow(data, plot_config)
     app.exec_()
-    return w.cropped_data_df
+    return w.cropped_data
 
 # use this function to include the visual data selection in a running pyqt app
-def select_visual_data_in_running_app(data_df, plot_config_dict, app):
-    w = MainWindow(data_df, plot_config_dict)
+def select_visual_data_in_running_app(data, plot_config : dict, app):
+    w = MainWindow(data, plot_config)
     app.processEvents()  # Process events once before waiting
     while w.isVisible():  # Loop until window is closed
         app.processEvents()
-    return w.cropped_data_df
+    return w.cropped_data
